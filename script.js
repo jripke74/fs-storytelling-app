c075aa9074749cc2e708db7158b153f2f4718675
Jeff Ripke <jripke74@gmail.com>
2025-03-30 10:14:01 -0700

    add several variables

---
+++ script.js
@@ -0,0 +1,19 @@
+// Step 6
+// In your JavaScript file, select the div with the class story-container and store it in a
+// variable called storyContainer.
+
+const storyContainer = document.querySelector(".story-container");
+
+// Step 7
+// Select the #scary-btn, #funny-btn, and #adventure-btn buttons and store them in variables called
+// scaryStoryBtn, funnyStoryBtn, and adventureStoryBtn.
+
+const scaryStoryBtn = document.querySelector("#scary-btn");
+const funnyStoryBtn = document.querySelector("#funny-btn");
+const adventureStoryBtn = document.querySelector("#adventure-btn");
+
+// Step 8
+// Select the #result paragraph and store it in a variable called resultParagraph. You will use this
+// variable to display the selected story.
+
+const resultParagraph = document.querySelector("#result");
