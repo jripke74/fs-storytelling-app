3a8b2e60f77f9bb784f8a39e3ab180a1132bf98f
Jeff Ripke <jripke74@gmail.com>
2025-03-31 07:16:17 -0700

    add storyObj

--- script.js
+++ script.js
@@ -17,3 +17,45 @@ const adventureStoryBtn = document.querySelector("#adventure-btn");
 // variable to display the selected story.
 
 const resultParagraph = document.querySelector("#result");
+
+// Step 9
+// To hold the different types of stories, create an object called storyObj. The object should have
+// three properties: scary, funny, and adventure. Each property value should be an object with a story
+// and a borderColor property.
+
+// Assign the following values to the individual objects:
+
+// scary:
+
+// story: In the dark woods, a group of friends stumbled upon an old, abandoned cabin. They enter the
+// cabin and awaken something malevolent that had been dormant for centuries.
+// borderColor: #ee4b2b
+// funny:
+
+// story: During a camping trip, Mark decided to show off his culinary skills by cooking dinner over an
+// open fire. However, his attempt caused him to burn the dinner as well as his eyebrows off.
+// borderColor: #f1be32
+// adventure:
+
+// story: Lost in the heart of the Amazon rain forest, Sarah and Jake stumbled upon an ancient temple.
+// They braved deadly traps and encountered strange wildlife, all while deciphering cryptic clues left
+// behind by a mysterious civilization.
+// borderColor: #acd157
+
+const storyObj = {
+  scary: {
+    story:
+      "In the dark woods, a group of friends stumbled upon an old, abandoned cabin. They enter the cabin and awaken something malevolent that had been dormant for centuries.",
+    borderColor: "#ee4b2b",
+  },
+  funny: {
+    story:
+      "During a camping trip, Mark decided to show off his culinary skills by cooking dinner over an open fire. However, his attempt caused him to burn the dinner as well as his eyebrows off.",
+    borderColor: "#f1be32",
+  },
+  adventure: {
+    story:
+      "Lost in the heart of the Amazon rain forest, Sarah and Jake stumbled upon an ancient temple. They braved deadly traps and encountered strange wildlife, all while deciphering cryptic clues left behind by a mysterious civilization.",
+    borderColor: "#acd157",
+  },
+};
